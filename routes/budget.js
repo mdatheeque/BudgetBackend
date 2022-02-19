e8f9a063697c2fa9f8d1c0d4bcacfa576f652dc3
mdatheeque <namaelite@gmail.com>
2022-02-20 01:18:10 +0530

    enhancements

--- routes/budget.js
+++ routes/budget.js
@@ -1,5 +1,7 @@
 //Getting express router
 const express = require("express");
+
+//importing all the controllers for budget from budget controllers
 const router = express.Router();
 const {
   addABudget,
@@ -10,11 +12,16 @@ const {
   getBudgetUsingFilters,
 } = require("../controllers/budget");
 
-router.post("/getbudgetssingfilters", getBudgetUsingFilters);
+//TODO: This has to be removed cause we can do the filter from the frontend itself from the central state
+router.post("/getbudgetusingfilters", getBudgetUsingFilters);
+
+//add, edit and delete budget routes
 router.post("/addabudget", addABudget);
 router.post("/editabudget", editaBudget);
 router.post("/deleteabudget", deleteABudget);
 
+//get all budgets and get sum of budget types to show the overview values in the Home page
+//TODO: Try filtering this value too from the data provided in central state
 router.get("/getallbudgets", getAllBudgets);
 router.get("/getsumofbudgettype", getSumOfBudgetTypes);
 
